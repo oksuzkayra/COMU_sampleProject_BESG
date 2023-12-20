af80f003507dc33eba01b18dc85d973d8cbfd767
oksuzkayra <oksuzkayra16@gmail.com>
2023-12-20 17:15:39 +0300

    edit

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://root:toor@mongodb:27017")
+db = client["search_db"]
+collection = db["datas"]
 
 app = FastAPI()
 
 @app.get("/search/{search_text}")
 def read_root(search_text):
-    return {"message": f"Hello, FastAPI!{search_text}"}
-
-@app.post("/search")
-def create_item(search_text):
-    return {"search_text_value": search_text}
+    result = collection.find({"Traffic Type": f"{search_text}"})
+    result_list = []
+    for i in result:
+        i["_id"] = None
+        result_list.append(i)
+    return result_list
+    
+#@app.post("/search")
+#def create_item(body:dict):
+#    return {"search_text_value": body["search_text"]}
